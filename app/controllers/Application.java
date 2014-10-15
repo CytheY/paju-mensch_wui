2707afbce463068ea1026b32f3f9d4f491650acd
palindel <palindel@htwg-konstanz.de>
2014-10-15 10:32:13 +0200

    BAEM OIDA

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,6 +8,8 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
+    	
+    	controller.Controller con = new controller.Controller();
         return ok(index.render("Your new application is ready."));
     }
 

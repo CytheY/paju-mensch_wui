f3d0e1c6a461b97d2abeccc22b22510e2acaa2d6
cyrus <cyrus@lapCyrus>
2014-11-25 12:40:26 +0100

    gamegrid html added

--- app/de/paju/mensch/play/controllers/Application.java
+++ app/de/paju/mensch/play/controllers/Application.java
@@ -1,11 +1,12 @@
 package de.paju.mensch.play.controllers;
 
-import de.paju.mensch.play.uis.WebTUI;
-import de.paju.mensch.observer.IObserver;
-import de.paju.mensch.controller.Controller;
 import play.mvc.Result;
+import de.paju.mensch.controller.Controller;
+import de.paju.mensch.play.uis.WebTUI;
 import de.paju.mensch.play.views.html.index;
+import de.paju.mensch.play.views.html.webgui;
 import de.paju.mensch.play.views.html.webtui;
+
 
 public class Application extends play.mvc.Controller {
 	
@@ -21,6 +22,11 @@ public class Application extends play.mvc.Controller {
     public static Result tui(String e, String input){
     	String output = tui.nextStep(e, input);
     	return ok(webtui.render(output));
+    }
+    
+    public static Result gameGrid(){
+    	game.getFieldCoords();
+    	return ok();
     }
 
 }
